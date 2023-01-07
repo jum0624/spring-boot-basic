8b0a673f73fb7eb99e676712512192e01bd281ab
jum0624 <jum0624@naver.com>
2023-01-08 02:32:56 +0900

    feat: ProductService 설계

---
+++ jpa/src/main/java/com/springboot/jpa/data/dto/ProductDTO.java
@@ -0,0 +1,17 @@
+package com.springboot.jpa.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ProductDTO {
+    private String name;
+    private int price;
+    private int stock;
+
+    public ProductDTO(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+}
